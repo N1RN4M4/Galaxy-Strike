51c0e59e89758bd581061a0d68ab9d70913216b1
N1RN4M4 <139238400+N1RN4M4@users.noreply.github.com>
2025-10-26 09:47:00 +0800

    Put onParticleCollision

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,1 +1,21 @@
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnParticleCollision(GameObject other)
+    {
+        Destroy(this.gameObject);
+    }
+
+    
+}
 
