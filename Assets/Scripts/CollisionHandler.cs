93f1793fe693c72d257cef81936864f5a32a70c0
N1RN4M4 <139238400+N1RN4M4@users.noreply.github.com>
2025-10-26 09:18:29 +0800

    Add onTriggerEnter function

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,1 +1,23 @@
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        // Debug.Log("Hit " + other.name);
+        Debug.Log($"Hit {other.gameObject.name}");
+    }
+}
 
