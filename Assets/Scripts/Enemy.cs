91d07e5c08bea6a6406ab806ce2c2c67a8935638
N1RN4M4 <139238400+N1RN4M4@users.noreply.github.com>
2025-10-26 10:41:25 +0800

    Insert the instantiate code, declare serializeable

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,18 +4,14 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject destroyedVFX;
 
     // Update is called once per frame
     void OnParticleCollision(GameObject other)
     {
+        Instantiate(destroyedVFX, transform.position, Quaternion.identity);
         Destroy(this.gameObject);
     }
 
     
 }
-
