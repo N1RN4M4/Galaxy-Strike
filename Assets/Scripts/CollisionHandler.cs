cad2f3c3663c5af497d9824c73db0a18f6900cc4
N1RN4M4 <139238400+N1RN4M4@users.noreply.github.com>
2025-10-26 09:45:46 +0800

    Clean the start and update function

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -2,22 +2,9 @@ using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-    
     void OnTriggerEnter(Collider other)
     {
         // Debug.Log("Hit " + other.name);
         Debug.Log($"Hit {other.gameObject.name}");
     }
 }
-
