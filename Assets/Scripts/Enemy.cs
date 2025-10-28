80c83a9feebae00c8ff6f5b0ca09b75b9fb74e3b
N1RN4M4 <139238400+N1RN4M4@users.noreply.github.com>
2025-10-28 11:59:51 +0800

    Declare HitPoints

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject destroyedVFX;
+    [SerializeField] int hitPoints = 3;
 
     // Update is called once per frame
     void OnParticleCollision(GameObject other)
