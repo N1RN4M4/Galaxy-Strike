690d6680e3f9a9d3f74c193eff1ecd672b92ec6c
N1RN4M4 <139238400+N1RN4M4@users.noreply.github.com>
2025-10-28 12:38:56 +0800

    Create base score function

--- Assets/Scripts/Scoreboard.cs
+++ Assets/Scripts/Scoreboard.cs
@@ -1,1 +1,13 @@
+using UnityEngine;
+
+public class Scoreboard : MonoBehaviour
+{
+    int score = 0;
+
+    void increaseScore(int points)
+    {
+        score += points;
+        Debug.Log("Score: " + score);
+    }
+}
 
